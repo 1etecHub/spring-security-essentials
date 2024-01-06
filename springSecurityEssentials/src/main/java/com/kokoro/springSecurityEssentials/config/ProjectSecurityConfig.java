c8b8b601851718676b47e652589584c381593934
Ebenezer <ebenirene5@gmail.com>
2024-01-07 00:40:22 +0100

    implemented password encoder for encoding passwords

--- springSecurityEssentials/src/main/java/com/kokoro/springSecurityEssentials/config/ProjectSecurityConfig.java
+++ springSecurityEssentials/src/main/java/com/kokoro/springSecurityEssentials/config/ProjectSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,8 +27,10 @@ public class ProjectSecurityConfig {
         /**
          *  Below is the custom security configurations
          */
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                        .requestMatchers("/notices", "/contact").permitAll())
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
