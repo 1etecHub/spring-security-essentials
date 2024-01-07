99b746e85f017decbe31cfc29e83dbaa6f98a3f0
Ebenezer <ebenirene5@gmail.com>
2024-01-07 22:01:50 +0100

    added authorities to the listof graanted authorities

--- springSecurityEssentials/src/main/java/com/kokoro/springSecurityEssentials/config/SecurityEssentialsUsernamePassworddAuthenticationProvider.java
+++ springSecurityEssentials/src/main/java/com/kokoro/springSecurityEssentials/config/SecurityEssentialsUsernamePassworddAuthenticationProvider.java
@@ -1,6 +1,7 @@
 package com.kokoro.springSecurityEssentials.config;
 
 
+import com.kokoro.springSecurityEssentials.model.Authority;
 import com.kokoro.springSecurityEssentials.model.Customer;
 import com.kokoro.springSecurityEssentials.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -34,15 +36,22 @@ public class SecurityEssentialsUsernamePassworddAuthenticationProvider implement
         List<Customer> customer = customerRepository.findByEmail(username);
         if (customer.size() > 0) {
             if (passwordEncoder.matches(pwd, customer.get(0).getPassword())) {
-                List<GrantedAuthority> authorities = new ArrayList<>();
-                authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole().name()));
-                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+                return new UsernamePasswordAuthenticationToken(username, pwd, getGrantedAuthorities(customer.get(0).getAuthorities()));
             } else {
                 throw new BadCredentialsException("Invalid password!");
             }
         }else {
             throw new BadCredentialsException("No user registered with this details!");
         }
+    }
+
+
+    private List<GrantedAuthority> getGrantedAuthorities(Set<Authority> authorities) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Authority authority : authorities) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getName()));
+        }
+        return grantedAuthorities;
     }
 
     @Override
--- springSecurityEssentials/src/main/java/com/kokoro/springSecurityEssentials/model/Customer.java
+++ springSecurityEssentials/src/main/java/com/kokoro/springSecurityEssentials/model/Customer.java
@@ -1,16 +1,17 @@
 package com.kokoro.springSecurityEssentials.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import com.kokoro.springSecurityEssentials.enums.Role;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -23,10 +24,20 @@ public class Customer {
     @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
     @GenericGenerator(name = "native",strategy = "native")
     private int id;
+
     private String email;
+
     private String fullName;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
     private Role role;
+
     private Boolean isVerified = false;
+
+    @JsonIgnore
+    @OneToMany(mappedBy="customer",fetch=FetchType.EAGER)
+    private Set<Authority> authorities;
 
 }
