8b47cd5fa85c4497efb4b6101b0c64a5eb5b07d6
Ebenezer <ebenirene5@gmail.com>
2024-01-10 23:18:39 +0100

    added application constansts

--- springSecurityEssentials/src/main/java/com/kokoro/springSecurityEssentials/config/ProjectSecurityConfig.java
+++ springSecurityEssentials/src/main/java/com/kokoro/springSecurityEssentials/config/ProjectSecurityConfig.java
@@ -23,6 +23,7 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @RequiredArgsConstructor
@@ -50,6 +51,7 @@ public class ProjectSecurityConfig {
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(Arrays.asList("Authorization"));
                         config.setMaxAge(3600L);
                         return config;
                     }
